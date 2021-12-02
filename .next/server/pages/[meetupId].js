85acc14d167617713be01a9a06437f323461cca5
Brian <brian@MacBook-Pro-de-Brian.local>
2021-12-02 18:45:17 -0500

    [Fixed]: Fallback

--- .next/server/pages/[meetupId].js
+++ .next/server/pages/[meetupId].js
@@ -202,7 +202,7 @@ async function getStaticPaths() {
   }).toArray();
   client.close();
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map(meetup => ({
       params: {
         meetupId: meetup._id.toString()
